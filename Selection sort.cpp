36efeb0ccf619b516c559986caa7c18f7b9c0d9e
Areeba4545 <123818284+Areeba4545@users.noreply.github.com>
2023-01-28 07:03:05 -0800

    Add files via upload

---
+++ Selection sort.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int arr[5]={5,3,7,6,4};
+for(int i=0;i<=4;i++)
+{ for(int j=i+1;j<=4;j++)
+   if(arr[i]>arr[j])
+   { int temp=arr[i];
+   arr[i]=arr[j];
+   arr[j]=temp;
+   }
+   cout<<arr[i];
+}
+}
+
